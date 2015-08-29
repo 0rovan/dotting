5867f896370b924b94a52364eef9f2ea6b1ec8d8
Michael Orovan <michael@orovan.co.uk>
2015-08-29 19:57:15 +0100

    init

---
+++ dotting.py
@@ -0,0 +1,38 @@
+from time import sleep
+from threading import Thread,Event
+from sys import stdout
+
+
+class Dotter(object):
+    def __init__(self,delay=100,symbol='.'):
+        self.event=Event()
+        self.delay=delay
+        self.symbol=symbol
+        self.status=False
+    def __loop(self):
+        while not self.event.is_set():
+            stdout.write(self.symbol)
+            stdout.flush()
+            sleep(self.delay/1000)
+    def start(self):
+        if not self.status:
+            self.event.clear()
+            Thread(target=self.__loop).start()
+            self.status=True
+    def stop(self,newLine=True):
+        if self.status:
+            self.event.set()
+            if newLine:
+                stdout.write('\n')
+            self.status=False
+    def __restart(self):
+        self.stop(False)
+        self.start()
+    def set(self,delay=None,symbol=None):
+        if delay!=None:
+            self.delay=delay
+            self.delay=delay
+        if symbol!=None:
+            self.symbol=symbol
+            self.symbol=symbol
+        self.__restart()
